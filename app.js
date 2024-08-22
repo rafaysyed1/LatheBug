89a251d0482f2399724e7d1a3b0c5af7ab3d16e1
Syed Muhammad Rafay Hassni <100311424+rafaysyed1@users.noreply.github.com>
2024-08-22 14:06:14 +0500

    Added routes and routes are working now

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const cookieParser = require("cookie-parser");
 const path = require('path');
 const db = require("./config/dbConfig")
 const ejs = require('ejs');
+const ownerRouter = require("./routes/ownerRouter");
+const userRouter = require("./routes/userRouter");
+const productRouter = require("./routes/productRouter");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine",ejs);
 
+app.use("/owner",ownerRouter);
+app.use("/user",userRouter);
+app.use("/products",productRouter);
 
 app.get("/",(req,res)=>{
         res.send("Server started!");
